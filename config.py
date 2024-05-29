d57c7da195ed379fdad2ce4f1663c43b18f46156
Thedestroyerte <mohdfarhan8859@gmail.com>
2024-05-29 13:31:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
 
 SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="5683212730").split()))
-for x in ALTRON:
+for x in DARK:
     SUDO_USERS.append(x)
 OWNER_ID = int(getenv("OWNER_ID", default="5683212730"))
 SUDO_USERS.append(OWNER_ID)
