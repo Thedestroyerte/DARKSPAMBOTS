5fb54f1a36de4958140fbb9bcf79b3a05b744af4
Thedestroyerte <mohdfarhan8859@gmail.com>
2024-05-29 16:38:41 +0530

    Update logs.py

--- AltBots/modules/logs.py
+++ AltBots/modules/logs.py
@@ -49,7 +49,7 @@ async def logs(legend):
         await asyncio.sleep(1)
 
         try:
-            await X1.send_file(legend.chat_id, "AltLogs.txt", caption=f"⚡ **DARKXBOTS LOGS** ⚡\n  » **ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:** `{ms} ꜱᴇᴄᴏɴᴅꜱ`")
+            await X1.send_file(legend.chat_id, "DarkLogs.txt", caption=f"⚡ **DARKXBOTS LOGS** ⚡\n  » **ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:** `{ms} ꜱᴇᴄᴏɴᴅꜱ`")
             await fetch.delete()
         except Exception as e:
             await fetch.edit(f"An Exception Occured!\n\n**ERROR:** {str(e)}")
