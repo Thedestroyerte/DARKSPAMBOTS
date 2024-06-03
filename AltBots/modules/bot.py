7c5751aed887fcc63d32ca05be2c81b4cbd96e9e
Thedestroyerte <mohdfarhan8859@gmail.com>
2024-06-03 19:03:53 +0530

    Update bot.py

--- AltBots/modules/bot.py
+++ AltBots/modules/bot.py
@@ -24,7 +24,7 @@ async def ping(e):
         altron = await e.reply(f"» __ᴀʟᴛʀᴏɴ__")
         end = datetime.now()
         mp = (end - start).microseconds / 1000
-        await altron.edit(f"⚡️ᴋɪsᴋɪ ɢᴀᴀɴᴅ ᴍᴀʀɴɪ ʜᴀɪ ʙᴏss⚡️_\n» `{mp} ᴍꜱ`")
+        await altron.edit(f"⚡️ＫＩＳＫＩ ＧＡＡＮＤ ＭＡＲＮＩ ＢＯＳＳ⚡️_\n» `{mp} ᴍꜱ`")
 
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
